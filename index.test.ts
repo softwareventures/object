5c7279fb1d370acd965f6b4d9ef619c01aec1ca9
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-10-11 14:26:59 +0100

    feat(map): add map function

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,7 @@
 import test from "ava";
+import {map} from "./index";
 
-test("placeholder test", t => t.true(true));
+test("map", t => {
+    t.deepEqual(map({a: 1, b: 2}, value => value + 1), {a: 2, b: 3});
+    t.deepEqual(map({a: 1, b: 2}, (value, key) => (value * 2) + key), {a: "2a", b: "4b"});
+});
--- index.ts
+++ index.ts
@@ -5,12 +5,23 @@ export function copy<T>(dictionary: ReadonlyDictionary<T>): Dictionary<T> {
     return {...dictionary};
 }
 
+// tslint:disable-next-line:no-unbound-method
 export const keys: <T>(dictionary: ReadonlyDictionary<T>) => string[] = Object.keys;
 
+// tslint:disable-next-line:no-unbound-method
 export const values: <T>(dictionary: ReadonlyDictionary<T>) => T[] = Object.values;
 
+// tslint:disable-next-line:no-unbound-method
 export const entries: <T>(dictionary: ReadonlyDictionary<T>) => Array<[string, T]> = Object.entries;
 
 export function empty<T>(dictionary: ReadonlyDictionary<T>): boolean {
     return keys(dictionary).length === 0;
+}
+
+export function map<T, U>(dictionary: ReadonlyDictionary<T>, f: (value: T, key: string) => U): Dictionary<U> {
+    const result: Dictionary<U> = {};
+    for (const [key, value] of entries(dictionary)) {
+        result[key] = f(value, key);
+    }
+    return result;
 }
