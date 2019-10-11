0dcd5868f85710984d6a59f01d4f555e5b903fd3
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-10-11 14:16:35 +0100

    feat: add keys, values and entries functions

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ import {Dictionary, ReadonlyDictionary} from "dictionary-types";
 export function copy<T>(dictionary: ReadonlyDictionary<T>): Dictionary<T> {
     return {...dictionary};
 }
+
+export const keys: <T>(dictionary: ReadonlyDictionary<T>) => string[] = Object.keys;
+
+export const values: <T>(dictionary: ReadonlyDictionary<T>) => T[] = Object.values;
+
+export const entries: <T>(dictionary: ReadonlyDictionary<T>) => Array<[string, T]> = Object.entries;
